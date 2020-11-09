15bb37d553ac662d7375c117e82bc34e648008f3
AbDev <ab.inpathtoadev@gmail.com>
2020-11-09 15:45:14 +0530

    heroku db

--- school_site/settings.py
+++ school_site/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 
 import django_heroku
+import dj_database_url
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -91,9 +92,12 @@ WSGI_APPLICATION = "school_site.wsgi.application"
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+    # "default": {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
+    "default": dj_database_url.config(conn_max_age=600, ssl_require=True)
 }
 
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES["default"].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
