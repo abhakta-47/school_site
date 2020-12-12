c71f667493775ab94dbfea29b5af89514400d2a1
AbDev <ab.inpathtoadev@gmail.com>
2020-12-12 11:18:51 +0530

    public : all links activated

--- home/urls.py
+++ home/urls.py
@@ -6,5 +6,11 @@ urlpatterns = [
     path("notices/", views.notices_view, name="notices"),
     path("admission/", views.admission, name="admission"),
     path("admission/fillup/", views.fillup, name="form fillup"),
+    path("courses/", views.courses, name="courses"),
+    path("teacher/", views.teacher, name="teacher"),
+    path("about/", views.about, name="about"),
+    path("pricing/", views.pricing, name="pricing"),
+    path("blog/", views.blog, name="blog"),
+    path("contact/", views.contact, name="contact"),
 ]
 
--- home/views.py
+++ home/views.py
@@ -39,6 +39,30 @@ def admission(request):
     return render(request, "admission.html")
 
 
+def courses(request):
+    return render(request,"courses.html")
+
+
+def teacher(request):
+    return render(request,"teacher.html")
+
+
+def about(request):
+    return render(request,"about.html")
+
+
+def pricing(request):
+    return render(request,"pricing.html")
+
+
+def blog(request):
+    return render(request,"blog.html")
+
+
+def contact(request):
+    return render(request,"contact.html")
+
+
 def fillup(request):
     if request.method != "POST":
         return redirect(admission)
